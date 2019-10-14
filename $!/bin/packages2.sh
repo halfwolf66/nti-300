f9c238fba17f0ba009c4b288d8854de5461da631
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2019-10-14 14:37:59 -0700

    Update packages2.sh

--- $!/bin/packages2.sh
+++ $!/bin/packages2.sh
@@ -3,7 +3,7 @@ if [ -e /usr/bin/wget ]; then
   exit 0;
 fi
 yum -y install wget #installs wget
-wget https://raw.githubusercontent.com/Sfenner/nti-300/master/package.txt
+wget https://raw.githubusercontent.com/halfwolf66/nti-300/master/package.txt
 for packages in $(cat packages.txt); do
   yum -y install $packages
 done
