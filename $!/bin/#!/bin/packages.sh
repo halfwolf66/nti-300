e4d85b79b4d1aa82997b74a59f0f739960740d49
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2019-10-14 14:44:13 -0700

    Update packages.sh

--- $!/bin/#!/bin/packages.sh
+++ $!/bin/#!/bin/packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum -y install wget
+wget https://github.com/halfwolf66/nti-300/edit/master/$!/bin/%23!/bin/packages.sh
 for packages in $(cat packages.txt); do 
   yum -y install $packages
 done
