7ae1034dbdb1a54265037b20c1fea7f0f5f519b0
halfwolf66 <55862988+halfwolf66@users.noreply.github.com>
2019-11-06 15:13:57 -0800

    Update django.sh

--- django.sh
+++ django.sh
@@ -12,7 +12,6 @@ source newenv/bin/activate
 django-admin startproject newproject
 cd newproject
 python manage.py migrate
-python manage.py createsuperuser #<-work around this to add a user
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'Pa$$w0rd')" | python manage.py shell
 newproject/settings.py #sed search and replace
 myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
